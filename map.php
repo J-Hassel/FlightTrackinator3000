1b6a6aaaba34a5543c95350d32cf87c730105f06
HexTeke <38196094+HexTeke@users.noreply.github.com>
2018-11-27 17:33:41 -0600

    finished working demo of map

--- map.php
+++ map.php
@@ -5,6 +5,48 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "flighttrackinator3000";
+
+// Establish
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+function mysqli_field_name($result, $field_offset)
+{
+    $properties = mysqli_fetch_field_direct($result, $field_offset);
+    return is_object($properties) ? $properties->name : null;
+}
+
+$aircraft_id = $_GET['aircraft_id'];
+$sql = "SELECT * FROM flight WHERE aircraft_id = '$aircraft_id'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+  echo "<center><h1> flight </h1>";
+  $count = mysqli_field_count($conn);
+  $header = "<table id='t01'><tr>";
+  for($x = 0; $x < $count; $x++){
+     $header = $header . "<th>" . mysqli_field_name($result, $x) . "</th>";
+  }
+  $header . "</tr>";
+  echo $header;
+   // output data of each row
+   while($row = $result->fetch_assoc()) {
+      $line = "<tr>";
+      for($x = 0; $x < $count; $x++){
+        $line = $line . "<td>" . $row[mysqli_field_name($result, $x)] . "</td>";
+      }
+      echo utf8_encode($line)."</tr>";
+      $latitude = $row['latitude'];
+      $longitude = $row['longitude'];
+   }
+   echo "</table></center>";
+   
+} else {
+   echo "0 results";
+}
+
+$conn->close();
 ?>
 <html>
   <head>
@@ -17,19 +59,21 @@ $dbname = "flighttrackinator3000";
     </style>
   </head>
   <body>
-    <h3>My Google Maps Demo</h3>
+    <h3> <?php echo $aircraft_id?> </h3>
     <!--The div element for the map -->
     <div id="map"></div>
     <script>
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
+  var latitude = "<?php echo $latitude ?>";
+  var longitude = "<?php echo $latitude ?>";
+  // The location of flightLocation
+  var flightLocation = {lat: parseFloat(latitude), lng: parseFloat(longitude)};
+  // The map, centered at flightLocation
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+      document.getElementById('map'), {zoom: 4, center: flightLocation});
+  // The marker, positioned at flightLocation
+  var marker = new google.maps.Marker({position: flightLocation, map: map});
 }
     </script>
     <script async defer
