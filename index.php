0f4ab4ff6a81d6ab5c54ee8b19ba8a6df15f80b3
Jeremy <jjdeanman@gmail.com>
2018-11-27 18:30:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -20,6 +20,11 @@ $dbname = "flighttrackinator3000";
           background-color: black;
           color: white;
       }
+      input {
+          width: 100%;
+          padding: 10px;
+          margin: 0px;
+      }
    </style>
 </head>
 <body>
@@ -35,7 +40,6 @@ $dbname = "flighttrackinator3000";
        <option value="airport">Airports</option>
       </select>
       </tr>
-
    
       <tr>
          <td>
@@ -53,6 +57,43 @@ function mysqli_field_name($result, $field_offset)
     return is_object($properties) ? $properties->name : null;
 }
 
+function build_table($result, $conn, $table_name){
+   if ($result->num_rows > 0) {
+      echo "<center><h1>" . $table_name . "</h1>";
+      $count = mysqli_field_count($conn);
+      $header = "<table id='t01'><tr>";
+      $line = "<tr><form method = \"post\" ><input type=\"hidden\" name = \"table_name\" value = \"$table_name\">";
+      for($x = 0; $x < $count; $x++){
+         $field_name = mysqli_field_name($result, $x);
+         $header = $header . "<th>$field_name</th>";
+         if($x > 0){
+            $line = $line . "</td>";
+         }
+         $line = $line . "<td><input name=\"$field_name\" type=\"text\" id=\"$field_name\">";
+      }
+      $line = $line . "<input name = \"Submit\" type = \"submit\" id = \"Submit\" value = \"Submit\"></form></tr>";
+      $header = $header . "</tr>";
+      echo $header;
+      echo $line;
+       // output data of each row
+       while($row = $result->fetch_assoc()) {
+          $line = "<tr>";
+         for($x = 0; $x < $count; $x++){
+            if($x == 0){
+               $aircraftID = $row[mysqli_field_name($result, $x)];
+               $line = $line . "<td><a href=\"map.php?aircraft_id=$aircraftID\">" . $row[mysqli_field_name($result, $x)] . "</a></td>";
+            }else{
+               $line = $line . "<td>" . $row[mysqli_field_name($result, $x)] . "</td>";
+            }
+         }
+         echo utf8_encode($line)."</tr>";
+       }
+       echo "</table></center>";
+   } else {
+       echo "0 results";
+   }
+}
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -61,29 +102,24 @@ if ($conn->connect_error) {
 } 
 if(isset($_POST['Submit'])) {
    $table_name = $_POST['table_name'];
-   $sql = "SELECT * FROM $table_name";
-   $result = $conn->query($sql);
-   if ($result->num_rows > 0) {
-      echo "<center><h1>" . $table_name . "</h1>";
-      $count = mysqli_field_count($conn);
-      $header = "<table id='t01'><tr>";
-      for($x = 0; $x < $count; $x++){
-         $header = $header . "<th>" . mysqli_field_name($result, $x) . "</th>";
-      }
-      $header . "</tr>";
-      echo $header;
-       // output data of each row
-       while($row = $result->fetch_assoc()) {
-          $line = "<tr>";
-         for($x = 0; $x < $count; $x++){
-            $line = $line . "<td>" . $row[mysqli_field_name($result, $x)] . "</td>";
+   if(count($_POST) > 2){
+      $sql = "SELECT * FROM $table_name";
+      $count = 0;
+      foreach ($_POST as $key => $value) {
+         if($key != "table_name" AND $key != "Submit"){
+            if($count == 0){
+               $sql = $sql . " WHERE $key LIKE '%$_POST[$key]%'";
+            }else{
+               $sql = $sql . " AND $key LIKE '%$_POST[$key]%'";
+            }
+            $count++;
          }
-         echo utf8_encode($line)."</tr>";
-       }
-       echo "</table></center>";
-   } else {
-       echo "0 results";
+      }
+   }else{
+      $sql = "SELECT * FROM $table_name";
    }
+   $result = $conn->query($sql);
+   build_table($result, $conn, $table_name);
    $conn->close();
 }
    ?>
