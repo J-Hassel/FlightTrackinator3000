59d780d58a922cd117567361d217d78d6f3f2ba5
HexTeke <38196094+HexTeke@users.noreply.github.com>
2018-11-28 16:46:31 -0500

    map now automatically zooms to fit all items

--- map.php
+++ map.php
@@ -123,6 +123,7 @@ $conn->close();
         anchor: new google.maps.Point(400, 400)	
       };
       var marker, i;
+      var bounds = new google.maps.LatLngBounds();
       for(i = 0; i < locations.length; i++){
           if(i > 0){
             marker = new google.maps.Marker({
@@ -139,7 +140,9 @@ $conn->close();
                 map: map
             });
           }
+          bounds.extend(marker.getPosition());
       }
+      map.fitBounds(bounds);
     };
     var names = [
         "<?php echo $aircraft_id?>",
