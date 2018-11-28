7bb7f8fc6ca891ca369c79ee7cce23c780bb9fba
HexTeke <38196094+HexTeke@users.noreply.github.com>
2018-11-28 16:37:39 -0500

    rearranged some elements

--- map.php
+++ map.php
@@ -13,7 +13,7 @@ $srcLng = "";
 $dstLat = "";
 $dstLng= "";
 
-// Establish
+// Establish connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
@@ -67,7 +67,6 @@ if ($result->num_rows > 0) {
 	  }
 
    }
-   echo "</table></center>";
    
 
    
@@ -75,15 +74,18 @@ if ($result->num_rows > 0) {
    echo "0 results";
 }
 
+
 $conn->close();
 ?>
 <html>
   <head>
+    <button onclick="history.go(-1);">Go Back</button>
     <style>
       /* Set the size of the div element that contains the map */
       #map {
-        height: 400px;  /* The height is 400 pixels */
-        width: 100%;  /* The width is the width of the web page */
+        margin: auto;
+        height: 600px;  /* The height is 400 pixels */
+        width: 85%;  /* The width is the width of the web page */
        }
        table#t01 tr:nth-child(even) {
           background-color: #eee;
@@ -109,8 +111,8 @@ $conn->close();
       var flightLocation = {lat: parseFloat(latitude), lng: parseFloat(longitude)};
       // The map, centered at flightLocation
       var map = new google.maps.Map(
-          document.getElementById('map'), {zoom: 4, center: flightLocation});
-      // The marker, positioned at flightLocation
+          document.getElementById('map'), {zoom: 4, center: flightLocation}
+      );
       
       var planeSymbol = {
         path: 'M362.985,430.724l-10.248,51.234l62.332,57.969l-3.293,26.145 l-71.345-23.599l-2.001,13.069l-2.057-13.529l-71.278,22.928l-5.762-23.984l64.097-59.271l-8.913-51.359l0.858-114.43 l-21.945-11.338l-189.358,88.76l-1.18-32.262l213.344-180.08l0.875-107.436l7.973-32.005l7.642-12.054l7.377-3.958l9.238,3.65 l6.367,14.925l7.369,30.363v106.375l211.592,182.082l-1.496,32.247l-188.479-90.61l-21.616,10.087l-0.094,115.684',
